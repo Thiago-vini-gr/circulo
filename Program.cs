01d65fbedc4df3147965a80ce0354dd0d3b41dcb
Thiago-vini-gr <Thiagaogamerrj@gmail.com>
2025-02-24 10:51:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definir o valor de Pi
+        double pi = 3.14159;
+
+        // Leitura do valor do raio
+        double raio = double.Parse(Console.ReadLine());
+
+        // Cálculo da área
+        double area = pi * raio * raio;
+
+        // Imprimir a área com 4 casas decimais
+        Console.WriteLine($"A={area:F4}");
+    }
+}
